0e5b55d0149b2d40e57e800dbff8ad9e4e7fc088
Pablo Baxter <pablobaxter@msn.com>
2023-03-21 22:15:52 -0700

    Upgrade Gradle and update build structure
    This also does some light cleanup of the project

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    `kotlin-dsl`
+    kotlin("jvm") version "1.8.10"
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(gradleKotlinDsl())
+    implementation(libs.bundles.android.build)
+    implementation(libs.kotlin.gradle.plugin)
+    implementation(libs.kotlin.dokka.plugin)
+    implementation(libs.androidx.navigation.safeargs.plugin)
+    implementation(libs.hilt.plugin)
+}
+
+gradlePlugin {
+    plugins {
+        create("frybitsAppPlugin") {
+            id = "frybits-application"
+            implementationClass = "com.frybits.harmony.gradle.FrybitsApplicationPlugin"
+        }
+
+        create("frybitsLibraryPlugin") {
+            id = "frybits-library"
+            implementationClass = "com.frybits.harmony.gradle.FrybitsLibraryPlugin"
+        }
+
+        create("frybitsTestPlugin") {
+            id = "frybits-test"
+            implementationClass = "com.frybits.harmony.gradle.FrybitsTestPlugin"
+        }
+    }
+}
+
---
+++ buildSrc/src/main/kotlin/com/frybits/gradle/PluginUtils.kt
@@ -0,0 +1,55 @@
+package com.frybits.gradle
+
+import com.android.build.api.dsl.BuildFeatures
+import com.android.build.api.dsl.BuildType
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.DefaultConfig
+import com.android.build.api.dsl.ProductFlavor
+import org.gradle.api.JavaVersion
+import org.gradle.api.plugins.ExtensionAware
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+
+/*
+ *  Copyright 2014 Prateek Srivastava
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * Created by Pablo Baxter (Github: pablobaxter)
+ */
+
+@Suppress("UnstableApiUsage")
+internal fun <BuildFeaturesT : BuildFeatures, BuildTypeT : BuildType, DefaultConfigT : DefaultConfig, ProductFlavorT : ProductFlavor>
+        CommonExtension<BuildFeaturesT, BuildTypeT, DefaultConfigT, ProductFlavorT>.configureCommonAndroid() {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 16
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunnerArguments["clearPackageData"] = "true"
+    }
+
+    testOptions {
+        execution = "ANDROIDX_TEST_ORCHESTRATOR"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    (this as? ExtensionAware)?.configure<KotlinJvmOptions> {
+        jvmTarget = "11"
+    }
+}
