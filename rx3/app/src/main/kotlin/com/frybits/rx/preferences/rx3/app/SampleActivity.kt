b633e32a4b89f18846ecd0175d927733b5a25189
Pablo Baxter <pablobaxter@msn.com>
2024-10-08 22:08:00 -0700

    Update source file licenses to reflect actuality of licenses

--- rx3/app/src/main/kotlin/com/frybits/rx/preferences/rx3/app/SampleActivity.kt
+++ rx3/app/src/main/kotlin/com/frybits/rx/preferences/rx3/app/SampleActivity.kt
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2014 Prateek Srivastava
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * Created by Pablo Baxter (Github: pablobaxter)
+ */
+
 package com.frybits.rx.preferences.rx3.app
 
 import android.os.Bundle
@@ -16,24 +34,6 @@ import com.frybits.rx.preferences.rx3.asObservable
 import com.google.common.base.Optional
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
-
-/*
- *  Copyright 2014 Prateek Srivastava
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- * Created by Pablo Baxter (Github: pablobaxter)
- */
 
 class SampleActivity : AppCompatActivity() {
 
